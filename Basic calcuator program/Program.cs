70efd4d6f43fcd70709cbfc79741d03e748e7dde
GBailey23 <96180369+TheGrinch1@users.noreply.github.com>
2023-01-27 01:15:47 -0800

    Basic calculator program initial upload

---
+++ Basic calcuator program/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bro_code_part_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
+
+
+                Console.WriteLine("******************");
+                Console.WriteLine("Calculator Program");
+                Console.WriteLine("******************");
+
+                Console.Write("Enter number 1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter an option: ");
+                Console.WriteLine("\t + to add ");
+                Console.WriteLine("\t - to subtract");
+                Console.WriteLine("\t * to multiply");
+                Console.WriteLine("\t / to divide");
+                Console.Write("Enter an option: ");
+
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("That was not a vaid option ");
+                        break;
+                }
+                Console.WriteLine("Would you like to continue? (Y/N)");
+            } while (Console.ReadLine().ToUpper() == "Y");
+            Console.WriteLine("Bye");
+        }
+    }
+}
